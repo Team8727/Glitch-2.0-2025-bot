a4972f301694be33e08d336a2f713997a6389c5c
ziggytron <ziggytron565@gmail.com>
2025-03-23 14:27:15 -0400

    improved pivot class

--- src/main/java/frc/robot/subsystems/AlgaeIntake/TestPivot.java
+++ src/main/java/frc/robot/subsystems/AlgaeIntake/TestPivot.java
@@ -1,3 +1,4 @@
+
 package frc.robot.subsystems.AlgaeIntake;
 
 import com.revrobotics.spark.config.SparkMaxConfig;
@@ -9,19 +10,20 @@ public class TestPivot extends Pivot {
   private static final double maxVelocity = 100;
   private static final double maxAcceleration = 100;
   private static final double zeroedAngelFromHorizontal = 0;
+  private static final double allowedError = 2;
   private static final int CANID = kAlgaeIntakePivot.intakePivotMotorCANID;
   private static final SparkMaxConfig config = new SparkMaxConfig();
   static {
     config
-        .smartCurrentLimit(40)
-        .idleMode(SparkMaxConfig.IdleMode.kBrake)
-        .inverted(false)
-        .closedLoop
-        .pid(0.5, 0, 0);
+      .smartCurrentLimit(40)
+      .idleMode(SparkMaxConfig.IdleMode.kBrake)
+      .inverted(false)
+      .closedLoop
+      .pid(0.5, 0, 0);
   }
 
   public TestPivot() {
-    super(config, CANID, zeroedAngelFromHorizontal, maxVelocity, maxAcceleration);
+    super(config, CANID, zeroedAngelFromHorizontal, maxVelocity, maxAcceleration, allowedError);
   }
 
   /** This method will be called once per scheduler run */
