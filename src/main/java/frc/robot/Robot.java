0d29d622ea3785736fc19cb1d0b0a541ddf21223
GriffinGryffinGryphon <griffinlowings@gmail.com>
2025-12-04 16:25:49 -0500

    some changes i made but forgot to commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,14 +13,12 @@ import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.Autos;
-import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Elevator.AlgaeRemover.AlgaeRemoverPivot;
 import frc.robot.subsystems.Elevator.AlgaeRemover.AlgaeRemoverRollers;
 import frc.robot.subsystems.Elevator.Coral.BackCoralRoller;
@@ -28,7 +26,7 @@ import frc.robot.subsystems.Elevator.Coral.FrontCoralRoller;
 import frc.robot.subsystems.Elevator.Elevator;
 import frc.robot.subsystems.GroundIntake.GroundIntakePivot;
 import frc.robot.subsystems.GroundIntake.GroundIntakeRollers;
-import frc.robot.subsystems.LEDs.LEDPatterns;
+import frc.robot.subsystems.LEDs.GlitchLEDPatterns;
 import frc.robot.subsystems.LEDs.LEDSubsystem;
 import frc.robot.pose.PoseEstimator;
 import frc.robot.vision.Vision;
@@ -51,7 +49,7 @@ public class Robot extends TimedRobot {
   private final PoseEstimator m_PoseEstimator = new PoseEstimator(m_SwerveSubsystem, m_Vision);
   private final Elevator m_elevator = new Elevator();
   private final LEDSubsystem m_ledSubsystem = LEDSubsystem.getInstance();
-  private final LEDPatterns m_ledPatterns = new LEDPatterns();
+  private final GlitchLEDPatterns m_ledPatterns = new GlitchLEDPatterns();
   private final NetworkTableLogger logger = new NetworkTableLogger("SHOW UPPPP");
   private final AlgaeRemoverRollers m_AlgeaRemoverRollers = new AlgaeRemoverRollers();
   private final AlgaeRemoverPivot m_AlgaeRemoverPivot = new AlgaeRemoverPivot();
@@ -154,7 +152,7 @@ public class Robot extends TimedRobot {
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {
-    m_ledSubsystem.setPattern(LEDPatterns.purple);
+    m_ledSubsystem.setPattern(GlitchLEDPatterns.purple);
   }
 
   /** This function is called periodically during disabled. */
@@ -166,7 +164,7 @@ public class Robot extends TimedRobot {
   public void autonomousInit() {
     CommandScheduler.getInstance().cancelAll();
     m_robotContainer.autonomousInit();
-    m_ledSubsystem.setPattern(LEDPatterns.rainbow);
+    m_ledSubsystem.setPattern(GlitchLEDPatterns.rainbow);
 
     m_Autos.selectAuto(); //Only enable this if you want the robot to do stuff during autonomous
 
@@ -181,8 +179,8 @@ public class Robot extends TimedRobot {
   public void teleopInit() {
     CommandScheduler.getInstance().cancelAll();
 
-    m_ledSubsystem.setPatternForDuration(LEDPatterns.theCoolerGreen, 0);
-
+    m_ledSubsystem.setPattern(GlitchLEDPatterns.fire(LEDSubsystem.defaultPattern));
+    
     // This makes sure that the autonomous stops running when
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
